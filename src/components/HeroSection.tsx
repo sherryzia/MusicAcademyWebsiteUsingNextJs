52612f4f7e9e033fbb55182e2a68e19c4823e103
Shaheer Zia Qazi <shaheerzia22@gmail.com>
2024-08-26 07:31:04 +0500

    Mobile view handled correctly

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ function HeroSection() {
     
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col justify-center items-center relative overflow-hidden">
       <div className="p-4 relative z-10 w-full text-center text-white mx-9">
-        <h1 className="text-7xl font-bold">Master the art of Music</h1>
+        <h1 className=" mt-28 text-7xl font-bold">Master the art of Music</h1>
         <p className="text-2xl mt-4 mx-9">
         Inspiring paragraph describing the world of music education. Learn how to master the art of music through various courses and professional guidance, designed for both beginners and advanced musicians.
         </p>
